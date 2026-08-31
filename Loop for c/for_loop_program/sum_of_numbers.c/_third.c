1495c0322291671931baa702dd24b6207ebf0081
Gauri <gaurinikure@gmail.com>
2026-08-31 14:35:50 +0530

    sum of even numbers

---
+++ Loop for c/for_loop_program/sum_of_numbers.c/_third.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+    int sum=0;
+     for(int i=1; i<=50; i++)
+     {
+        if(i%2==0)
+        {
+            sum=sum+i;
+        }
+     }
+     printf("Sum of first 50 even numbers is %d\n",sum);
+     
+     return 0;
+}
