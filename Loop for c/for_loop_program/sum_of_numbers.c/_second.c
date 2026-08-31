c7d716ba8bb39378d8d54c2f20a668bb62d50fae
Gauri <gaurinikure@gmail.com>
2026-08-31 14:28:58 +0530

    sum of first 100 nnumbers

---
+++ Loop for c/for_loop_program/sum_of_numbers.c/_second.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+    int sum=0;
+     for(int i=1; i<=100; i++)
+     {
+        sum=sum+i;
+     }
+     printf("Sum of first 100 numbers is %d\n",sum);
+     
+     return 0;
+}
