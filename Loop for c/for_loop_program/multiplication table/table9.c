f988e181500b3e932cc5a73045af39cc51cfaa39
Gauri <gaurinikure@gmail.com>
2026-09-01 13:44:39 +0530

    only even multiples will print

---
+++ Loop for c/for_loop_program/multiplication table/table9.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main()
+{
+     int n, i;
+     
+     printf(" Enter a number: ");
+     scanf("%d", &n);
+
+     printf("Multiplication table of %d is:\n", n);
+     for( i=1;i<=10;i++)
+     if ((n*i)%2==0)   
+     {
+     {
+        printf("%d*%d = %d\n", n,i, n*i);
+     }
+      }
+
+     return 0;
+}
