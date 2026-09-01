1283a801ebb861f180513cf3e2c06d62ca1a6ea8
Gauri <gaurinikure@gmail.com>
2026-09-01 14:07:52 +0530

    taking number from user

---
+++ Loop for c/for_loop_program/multiplication table/table10.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,m ;
+    int table;
+
+    printf("Enter the starting table number: ");
+    scanf("%d", &n);
+    printf("Enter the ending table number: ");
+    scanf("%d", &m);
+
+    for(int table = n; table <= m; table++)
+{
+    for(int i = 1; i <= 10; i++)
+    {
+        if((table * i) % 2 == 0)
+        {
+            printf("%d * %d = %d\n", table, i, table * i);
+        }
+    }
+}
+
+return 0;
+}
